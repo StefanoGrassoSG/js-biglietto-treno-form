a79829179ed24aa80ce209132b770787c11522f4
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-13 19:38:20 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@
 *************/
 
 const price = 0.21;
-
 const outPut = document.getElementById('price')
+const wagon = document.getElementById('wagon')
+const code = document.getElementById('CP')
+const passenger = document.getElementById('pass')
 
 /***************
      BUTTON
@@ -13,9 +15,11 @@ const outPut = document.getElementById('price')
 const myButton = document.querySelector('[value="Generates"]');
 myButton.addEventListener('click',
     function() {
+        const nameInput = document.querySelector('[name="name"]')
         const kmInput = document.querySelector('[name="km"]');
         const ageInput = document.querySelector('[name="age"]');
 
+        const nameOutput = nameInput.value;
         const kmOutput = kmInput.value;
         const ageOutput = ageInput.value;
         
@@ -29,7 +33,16 @@ myButton.addEventListener('click',
             finalPrice = finalPrice * 0.6;
         }
 
+        const random = Math.floor(Math.random() * 10) + 1;
+
+        const cpRandom = Math.floor(Math.random() * 10000) + 90000;
+
+        passenger.innerHTML = nameOutput;
+
         outPut.innerHTML = finalPrice.toFixed(2) + '€';
-        
+
+        wagon.innerHTML = random;
+        code.innerHTML = cpRandom;
+
     }
 )   
